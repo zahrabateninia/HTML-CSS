622f11709fa423a2cfa7c9c8c735e91857d23081
zahrabateninia <zahrabatenin@gmail.com>
2023-09-08 23:02:08 +0330

    using form controls outside of forms

---
+++ forms/greet.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const name = document.querySelector("#username")
+    const greetMeButton = document.querySelector(".greet-btn")
+    const greetingOutput = document.querySelector(".greeting")
+
+    greetMeButton.addEventListener('click', (event) => {
+    greetingOutput.innerText = `Hello ${name.value}`;
+    })    
+});
